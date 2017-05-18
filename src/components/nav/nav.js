90828fc028fa2e83bcd4bc7df00b25e92e68249f
chloe <chloe@hanyus-MacBook-Pro.local>
2017-05-18 14:01:51 -0700

    Modify routers.

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -11,7 +11,7 @@ class Nav extends Component {
       {
         "id" : 1,
         "text": "About",
-        "url": "/#/about"
+        "url": "#/about"
       },
       {
         "id" : 2,
--- src/routes.js
+++ src/routes.js
@@ -15,10 +15,10 @@ import Connect from 'containers/Connect/Connect';
 
 export default (
   <Router history={hashHistory}>
-    <Route path="/" component={App}>
+    <Route path="#/" component={App}>
       <IndexRoute path="" component={Home}/>
     </Route>
-    <Route path="./about" component={About}></Route>
+    <Route path="#/about" component={About}></Route>
     <Route path="/design" component={Design}></Route>
     <Route path="/design/01" component={FindMe}></Route>
     <Route path="/design/02" component={Udacity}></Route>
@@ -26,6 +26,6 @@ export default (
     <Route path="/art" component={Art}></Route>
     <Route path="/art/01" component={Art1}></Route>
     <Route path="/code" component={Code}></Route>
-    <Route path="portfolio-site/#/connect" component={Connect}></Route>
+    <Route path="/connect" component={Connect}></Route>
   </Router>
 );
