48a50e74d881fd2ce86acecaf6788e3625405452
Hanyu Jiang <ptchiang12@gmail.com>
2018-02-13 22:42:07 -0800

    Remove particles.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -8,40 +8,6 @@ class App extends Component {
   render() {
     return (
       <DocumentTitle title='HÀNYǓ'>
-        <div>
-          <Particles params={{
-              		particles: {
-                    "number": {
-                      "value": 200,
-                      "density": {
-                        "enable": true,
-                        "value_area": 800
-                      }
-                    },
-                    size: {
-                      "value": 20,
-                      "random": true,
-                      "anim": {
-                        "enable": false,
-                        "speed": 180,
-                        "size_min": 0.1,
-                        "sync": false
-                      }
-                    },
-              			line_linked: {
-                      "enable": true,
-                      "distance": 250,
-                      "color": "#000",
-                      "opacity": 1,
-                      "width": 2,
-              				shadow: {
-              					enable: true,
-              					color: "#FF1493",
-              					blur: 0
-              				}
-              			}
-              		}
-                }} />
           {this.props.children}
         </div>
      </DocumentTitle>
